fb5d3e68700544f44f44fedf804873faae238617
Sree Lakshmi <sinisreelaksmi19@gmail.com>
2025-03-08 20:44:23 +0530

    add delete

--- script.js
+++ script.js
@@ -3,15 +3,36 @@ const list = document.getElementById('list');
 const btn = document.getElementById('btn');
 const err = document.getElementById('err');
 
-btn.addEventListener('click', () => {
+function addTodo() {
     if (todo.value === "") {
         err.innerText = "Please enter a todo!";
         err.style.color = "red";
     } else {
         const li = document.createElement('li');
+        const button = document.createElement('button');
+        button.innerText = "Delete";
+        const div = document.createElement('div');
         li.innerText = todo.value;
-        list.appendChild(li);
+        div.appendChild(li);
+        div.appendChild(button);
+        list.appendChild(div);
+        li.addEventListener('click', () => {
+            li.classList.toggle("finish");
+        });
+        button.addEventListener('click', (e) => {
+            e.target.parentElement.remove();
+        });
         todo.value = "";
         err.innerText = "";
     }
-})
+}
+
+btn.addEventListener('click', () => {
+    addTodo();
+});
+
+todo.addEventListener('keypress', (e) => {
+    if (e.code == "Enter") {
+        addTodo();
+    }
+});
